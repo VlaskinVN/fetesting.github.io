8c45f7b001dfa6050d9d74452cf11b1a52fd6299
nicolaevich.vv.dev <nicolaevich.vv.dev@gmail.com>
2020-08-02 10:56:55 +0300

    add addaptive fotter

--- js/script.js
+++ js/script.js
@@ -8,37 +8,27 @@ $(function() {
             nav: true,
             dots: true,
             dotsData: true,
-        });
-        
-    });
-    $(document).ready(function() {        
-        var $navbar = $("#mNavbar");  
-     
-        
-        AdjustHeader(); // Incase the user loads the page from halfway down (or something);
-        $(window).scroll(function() {
-            AdjustHeader();
-        });
-    
-    function AdjustHeader(){
-      if ($(window).scrollTop() > 70) {
-        if (!$navbar.hasClass("navbar-fixed-top")) {
-          $navbar.addClass("navbar-fixed-top");
-        }
-      } else {
-        $navbar.removeClass("navbar-fixed-top");
-      }
-    }
+        });        
+});
+$(document).ready(function() {    
+  var $navbar = $("#mNavbar");          
+  AdjustHeader(); // Incase the user loads the page from halfway down (or something);
+  $(window).scroll(function() {
+    AdjustHeader();
   });
+    
+  function AdjustHeader(){
+    if ($(window).scrollTop() > 70) {
+      if (!$navbar.hasClass("navbar-fixed-top")) {
+        $navbar.addClass("navbar-fixed-top");
+      }
+    } else {
+      $navbar.removeClass("navbar-fixed-top");
+    }
+  }
+});
   
 function resize() {
-    if(window.outerWidth <= 992){
-        document.getElementById("top").style.display = "none";
-        document.getElementById("fserch").style.display = "block";
-    }else{
-        document.getElementById("top").removeAttribute("style");
-        document.getElementById("fserch").style.display = "none";
-    }
     if(window.outerWidth >= 992) {
         document.getElementById("fserch").style.display = "none";
     }
